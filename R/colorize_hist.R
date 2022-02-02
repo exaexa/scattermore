6cc64721cf8a5dbd97af34b7adc899038a6f440a
Tereza Kulichova <kulichova.t@gmail.com>
2022-02-02 20:58:44 +0100

    rename

--- R/colorize_hist.R
+++ R/colorize_hist.R
@@ -4,8 +4,8 @@
 #'
 #' @param hist matrix or array R datatype interpreted as histogram
 #'
-#' @param pallete matrix (4xn dim, n>= 2) with R, G, B and alpha channels 
-#'                in integers, defaults to shades of `red`, `green` and `white` with `alpha = 255`
+#' @param rgba matrix (4xn dim, n>= 2) with R, G, B and alpha channels 
+#'                in integers, defaults to shades of `red`, `green` and `blue` with `alpha = 255`
 #'
 #' @return float matrix with the result.
 #'
@@ -14,14 +14,14 @@
 #' @importFrom grDevices as.raster
 colorize_hist <- function(
   hist,
-  palette = array(c(250,128,114,255,144,238,144,255,176,224,230,255), c(4,3)))
+  rgba = array(c(250,128,114,255,144,238,144,255,176,224,230,255), c(4,3)))
 {
    rows = dim(hist)[1]
    cols = dim(hist)[2]
-   size = dim(palette)[2]
+   size = dim(rgba)[2]
    
    if(!is.matrix(hist)) stop('histogram in matrix form expected')
-   if(dim(palette)[1] != 4) stop('palette with 4 columns expected')
+   if(dim(rgba)[1] != 4) stop('palette with 4 columns expected')
    if(size < 2) stop('palette with at least 2 colors expected')
    
    matrix <- rep(0, rows * cols * 4)
@@ -30,7 +30,7 @@ colorize_hist <- function(
    result <- .C("hist_colorize",
      dimen = as.integer(c(rows, cols, size)),
      matrix = as.integer(matrix),
-     palette = as.integer(palette),
+     rgba = as.integer(rgba),
      normalized_hist = as.single(normalized_hist))
 
     colorized_hist = array(result$matrix, c(rows, cols, 4))
