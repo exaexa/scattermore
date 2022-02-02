d1015aa1aad9c635742f20251212c940da7d74c2
Tereza Kulichova <kulichova.t@gmail.com>
2022-02-02 15:14:54 +0100

    add functionality color hist (floats)

--- src/hist_colorize.c
+++ src/hist_colorize.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+#include <math.h>
 
 //colorize histogram with given color palette
 void
 hist_colorize(const int *dim,
-	      float *matrix,
-	      const float *pallete,
-	      const float *data)
+	      float *matrix,         //change?
+	      const float *palette,  //change?
+	      const float *hist)
 {
-
+	const size_t rows = dim[0], cols = dim[1], size_palette = dim[2], size_hist = rows * cols,
+	             offset_G = size_hist, offset_B = offset_G * 2, offset_A = offset_G * 3;
+	const float bin = 1.0/size_palette;
+	
+	size_t i;
+	for(i = 0; i < rows; ++i)
+	{
+	  size_t j;
+	  for(j = 0; j < cols; ++j)
+	  {
+	  	float hist_value = hist[j*rows + i];
+	  	size_t palette_index = ((size_t)(hist_value / bin));  //determining column in palette
+	  	
+	  	if(palette_index == size_palette) --palette_index;
+	  	
+	  	float R = palette[4 * palette_index + 0];
+	  	float G = palette[4 * palette_index + 1];
+	  	float B = palette[4 * palette_index + 2];
+	  	float A = palette[4 * palette_index + 3];
+	  	
+	  	size_t offset = j*rows + i;
+	  	matrix[offset] = R;
+	  	matrix[offset + offset_G] = G;
+	  	matrix[offset + offset_B] = B;
+	  	matrix[offset + offset_A] = A;
+	  }
+	}    
 
 }
