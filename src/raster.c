13fdc5197c6ef58617f30f29b8fd0002520ad78b
Tereza Kulichova <kulichova.t@gmail.com>
2022-02-03 14:11:29 +0100

    add c raster function

---
+++ src/raster.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//colorize histogram with given color palette
+void
+raster(const unsigned *dim,
+       float *matrix,
+       const float *rgba,
+       const float *xy)
+{
+
+
+}
