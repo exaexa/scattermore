dc6268ad918ef25107bfd208e819359d08dc5aaf
Tereza Kulichova <56218654+teri934@users.noreply.github.com>
2022-04-02 22:38:42 +0200

    Update hist_int.c

--- src/hist_int.c
+++ src/hist_int.c
@@ -3,13 +3,13 @@
 //create histogram from given point coordinates in respect to output size
 void
 hist_int(const unsigned *pn,
-	     const unsigned *size_out,
-	     unsigned *histogram,
-	     const float *xlim,
-	     const float *ylim,
-	     const float *xy)
+	 const unsigned *size_out,
+	 unsigned *histogram,
+	 const float *xlim,
+	 const float *ylim,
+	 const float *xy)
 {
-	const size_t size_data = *pn;
+    const size_t size_data = *pn;
     const size_t size_out_x = size_out[0];
     const size_t size_out_y = size_out[1];
 
@@ -20,10 +20,10 @@ hist_int(const unsigned *pn,
     const float y_begin = ylim[1];
     const float y_end = ylim[0];
     const float y_bin = (size_out_y - 1) / (y_end - y_begin);
-        
-        
+	
+
     size_t i;
-    for(i = 0; i < size_data; ++i)
+    for (i = 0; i < size_data; ++i)
     {
         size_t x = (xy[i] - x_begin) * x_bin;  //get new point coordinates for histogram
         size_t y = (xy[i + size_data] - y_begin) * y_bin;
