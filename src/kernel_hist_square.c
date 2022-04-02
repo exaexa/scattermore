5662e917c100141f2952c9a3f3423f4fea5c5058
Tereza Kulichova <56218654+teri934@users.noreply.github.com>
2022-04-02 22:32:09 +0200

    Update kernel_hist_square.c

--- src/kernel_hist_square.c
+++ src/kernel_hist_square.c
@@ -4,20 +4,20 @@
 //blur histogram using square kernel of ones
 void
 kernel_hist_square(const unsigned *dim,
-	            const float *kernel,
-	            float *blurred_histogram,
-	            const float *histogram)
+	           const float *kernel,
+	           float *blurred_histogram,
+	           const float *histogram)
 {
-	const size_t size_out_y = dim[0];
+    const size_t size_out_y = dim[0];
     const size_t size_out_x = dim[1];
 	
-	size_t i;
-	for(i = 0; i < size_out_y; ++i)
-	{
+    size_t i;
+    for (i = 0; i < size_out_y; ++i)
+    {
         size_t j;
-        for(j = 0; j < size_out_x; ++j)
+        for (j = 0; j < size_out_x; ++j)
         {
-            blurred_histogram[j*size_out_y + i] = blur(kernel, histogram, dim, j, i); //blurring of given point
+            blurred_histogram[j * size_out_y + i] = blur(kernel, histogram, dim, j, i); //blurring of given point
         }
-	}  
+    }  
 }
