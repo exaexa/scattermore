cda7533fcdbf11daee7415a1130751dea9470375
Tereza Kulichova <56218654+teri934@users.noreply.github.com>
2022-04-02 22:45:24 +0200

    Update kernel_hist_gauss.c

--- src/kernel_hist_gauss.c
+++ src/kernel_hist_gauss.c
@@ -8,20 +8,20 @@ kernel_hist_gauss(const unsigned *dim,
 	          const float *histogram,
 	          const float *sigma)
 {
-	const size_t size_out_y = dim[0];
+    const size_t size_out_y = dim[0];
     const size_t size_out_x = dim[1];
     const size_t size_kernel = dim[2];
-	float kernel[size_kernel * size_kernel];
+    float kernel[size_kernel * size_kernel];
 
-	create_gauss(kernel, size_kernel, *sigma);
+    create_gauss(kernel, size_kernel, *sigma);
 	
-	size_t i;
-	for(i = 0; i < size_out_y; ++i)
-	{
+    size_t i;
+    for (i = 0; i < size_out_y; ++i)
+    {
         size_t j;
-        for(j = 0; j < size_out_x; ++j)
+        for (j = 0; j < size_out_x; ++j)
         {
             blurred_histogram[j * size_out_y + i] = blur(kernel, histogram, dim, j, i); //blurring of given point
         }
-	}    
+    }    
 }
