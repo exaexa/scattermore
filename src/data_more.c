5376bcea8d7c7772ac0166bbab4ac5ce25e1bfb7
Tereza Kulichova <56218654+teri934@users.noreply.github.com>
2022-04-02 22:51:04 +0200

    Update data_more.c

--- src/data_more.c
+++ src/data_more.c
@@ -21,7 +21,7 @@ data_more(const unsigned *dim,
     const size_t offset_T = size_out * 4;
     const size_t offset_RGBA = 4;
 	             
-	const float x_begin = xlim[0];
+    const float x_begin = xlim[0];
     const float x_end = xlim[1];
     const float x_bin = (size_out_x - 1) / (x_end - x_begin);
 
@@ -30,25 +30,25 @@ data_more(const unsigned *dim,
     const float y_bin = (size_out_y - 1) / (y_end - y_begin);
                                 
 		  	
-	size_t i;
-	for(i = 0; i < size_data; ++i)
-	{
-		size_t x = (xy[i] - x_begin) * x_bin;  //get new point coordinates for result raster
-		size_t y = (xy[i + size_data] - y_begin) * y_bin;
-			
-		if(x >= size_out_x || y >= size_out_y)
-			continue;		
-			
-		float R = RGBA[offset_RGBA * i + 0];
-		float G = RGBA[offset_RGBA * i + 1];
-		float B = RGBA[offset_RGBA * i + 2];
-		float A = RGBA[offset_RGBA * i + 3];
-				
-		size_t offset = x * size_out_y + y;
-		RGBWT[offset + offset_R] += R * A;
-		RGBWT[offset + offset_G] += G * A;
-		RGBWT[offset + offset_B] += B * A;
-		RGBWT[offset + offset_W] += A;
-		RGBWT[offset + offset_T] *= 1 - A;
-	}	 
+    size_t i;
+    for (i = 0; i < size_data; ++i)
+    {
+        size_t x = (xy[i] - x_begin) * x_bin;  //get new point coordinates for result raster
+	size_t y = (xy[i + size_data] - y_begin) * y_bin;
+	    
+	if (x >= size_out_x || y >= size_out_y)
+		continue;		
+
+        float R = RGBA[offset_RGBA * i + 0];
+	float G = RGBA[offset_RGBA * i + 1];
+	float B = RGBA[offset_RGBA * i + 2];
+	float A = RGBA[offset_RGBA * i + 3];
+	    
+	size_t offset = x * size_out_y + y;
+	RGBWT[offset + offset_R] += R * A;
+	RGBWT[offset + offset_G] += G * A;
+	RGBWT[offset + offset_B] += B * A;
+	RGBWT[offset + offset_W] += A;
+	RGBWT[offset + offset_T] *= 1 - A;
+    }	 
 }
