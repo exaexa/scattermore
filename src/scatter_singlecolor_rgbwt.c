abcd091306d229f499f7bb08e3aaf2c0c20c0be9
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-04-13 09:54:47 +0200

    add C code licenses, cleanup includes

--- src/scatter_singlecolor_rgbwt.c
+++ src/scatter_singlecolor_rgbwt.c
@@ -1,4 +1,25 @@
-#include <stdio.h>
+
+/*
+ * This file is part of scattermore.
+ *
+ * Copyright (C) 2022 Mirek Kratochvil <exa.exa@gmail.com>
+ *               2022 Tereza Kulichova <kulichova.t@gmail.com>
+ *
+ * scattermore is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * scattermore is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * scattermore. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "scatters.h"
 
 // calculate RGBWT matrix with one given color
 void
--- src/scatters.h
+++ src/scatters.h
@@ -1,6 +1,26 @@
-#ifndef SCATTERS_H_
-#define SCATTERS_H_
-#include <stdio.h>
+
+/*
+ * This file is part of scattermore.
+ *
+ * Copyright (C) 2022 Mirek Kratochvil <exa.exa@gmail.com>
+ *               2022 Tereza Kulichova <kulichova.t@gmail.com>
+ *
+ * scattermore is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * scattermore is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * scattermore. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef SCATTERS_H
+#define SCATTERS_H
 
 void
 scatter_histogram(const unsigned *pn,
