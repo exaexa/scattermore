66ec30818d98ccd4b3870b5bd41eb171052f839e
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-04-13 11:45:52 +0200

    let's have actual 3 tests

--- tests/testthat/test-scattermoreplot-basic.R
+++ tests/testthat/test-scattermoreplot-basic.R
@@ -1,4 +1,8 @@
 
 test_that("scattermoreplot does not fail on trivial data", {
-  expect_silent(scattermoreplot(rnorm(10), rnorm(10), col = rainbow(10)))
+  expect_silent(scattermoreplot(rnorm(10), rnorm(10)))
+
+  expect_silent(scattermoreplot(rnorm(10), rnorm(10), col = "red"))
+
+  expect_silent(scattermoreplot(rnorm(10), rnorm(10), col = rainbow(10), xlim = c(-3, 3), ylim = c(-3, 3)))
 })
