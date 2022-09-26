06fe3e2b4f20df1fd1ba2fc7a843609115943a84
teri934 <kulichova.t@gmail.com>
2022-09-26 11:09:07 +0200

    first SIMD

--- src/histogram_to_rgbwt.c
+++ src/histogram_to_rgbwt.c
@@ -19,8 +19,11 @@
  * scattermore. If not, see <https://www.gnu.org/licenses/>.
  */
 
+// COMPILE WITH -MSSE4
 #include "scatters.h"
 
+#include <xmmintrin.h>
+#include <intrin.h>
 #include <stddef.h>
 
 // colorize histogram with given color palette
@@ -42,28 +45,53 @@ histogram_to_rgbwt(const unsigned *dim,
   const size_t offset_W = size_out * 3;
   const size_t offset_T = size_out * 4;
 
-  size_t i;
-  for (i = 0; i < size_out_y; ++i) {
-    size_t j;
-    for (j = 0; j < size_out_x; ++j) {
-      float histogram_value = histogram[j * size_out_y + i];
-      size_t palette_index =
-        ((size_t)(histogram_value / bin)); // determining column in palette
+  const float *histogram_end = histogram + size_out;
+  const float *histogram_end_rest = histogram_end - 3;  //multiplier of 4
+  const __m128 bins = _mm_set1_ps(bin);
+  const __m128 ones = _mm_set1_ps(1);
+  const __m128i fours = _mm_set1_epi32(4);
+  const __m128i sizes_palette = _mm_set1_epi32(size_palette - 1);
 
-      if (palette_index == size_palette)
-        --palette_index;
+  for(; histogram < histogram_end_rest; histogram += 4, RGBWT += 4) {
+    const __m128 histogram_values = _mm_loadu_ps(histogram);
+    // palette indices multiplied by 4
+    __m128i palette_indices = _mm_mul_epi32(_mm_castps_si128(_mm_div_ps(histogram_values, bins)), fours);
+    // correct if one of the indices is equal to size_palette
+    palette_indices = _mm_min_epi32(palette_indices, sizes_palette);
 
-      float R = palette[4 * palette_index + 0];
-      float G = palette[4 * palette_index + 1];
-      float B = palette[4 * palette_index + 2];
-      float A = palette[4 * palette_index + 3];
+    // load RGBA values
+    __m128 data_0 = _mm_load_ps(palette + _mm_extract_epi32(palette_indices, 0) * 4);
+    __m128 data_1 = _mm_load_ps(palette + _mm_extract_epi32(palette_indices, 1) * 4);
+    __m128 data_2 = _mm_load_ps(palette + _mm_extract_epi32(palette_indices, 2) * 4);
+    __m128 data_3 = _mm_load_ps(palette + _mm_extract_epi32(palette_indices, 3) * 4);
+    // transpose and get SoA format
+    _MM_TRANSPOSE4_PS(data_0, data_1, data_2, data_3);
 
-      size_t offset = j * size_out_y + i;
-      RGBWT[offset + offset_R] = R;
-      RGBWT[offset + offset_G] = G;
-      RGBWT[offset + offset_B] = B;
-      RGBWT[offset + offset_W] = 1;
-      RGBWT[offset + offset_T] = 1 - A;
-    }
+    _mm_store_ps(RGBWT + offset_R, data_0);
+    _mm_store_ps(RGBWT + offset_G, data_1);
+    _mm_store_ps(RGBWT + offset_B, data_2);
+    _mm_store_ps(RGBWT + offset_W, ones);
+    _mm_store_ps(RGBWT + offset_T, _mm_sub_ps(ones, data_3)); //RGBWT[offset + offset_T] = 1 - A;
+  }
+
+  for(; histogram < histogram_end; ++histogram, ++RGBWT){  //do the rest values
+    float histogram_value = *histogram;
+    size_t palette_index =
+      ((size_t)(histogram_value / bin)); // determining column in palette
+
+    if (palette_index == size_palette)
+      --palette_index;
+
+    float R = palette[4 * palette_index + 0];
+    float G = palette[4 * palette_index + 1];
+    float B = palette[4 * palette_index + 2];
+    float A = palette[4 * palette_index + 3];
+
+    // don't add offset
+    RGBWT[offset_R] = R;
+    RGBWT[offset_G] = G;
+    RGBWT[offset_B] = B;
+    RGBWT[offset_W] = 1;
+    RGBWT[offset_T] = 1 - A;
   }
 }
